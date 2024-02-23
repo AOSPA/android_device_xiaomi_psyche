f53dcbe9e677f63465be694ac85b97902b98f2d6
hiper25 <2243168976@qq.com>
2024-02-23 11:32:43 +0800

    psyche: Kang regional model identification from marble
     It's because some models are optimized for certain regions.
    
    Change-Id: I3a22911828913a838adff925491cbb73788c6da2

--- init/init_psyche.cpp
+++ init/init_psyche.cpp
@@ -1,35 +1,80 @@
 /*
- * Copyright (C) 2022 Paranoid Android
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#include <vector>
+   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (C) 2019-2020 The LineageOS Project.
+   Copyright (C) 2021 The Android Open Source Project.
+   Copyright (C) 2022-2023 Paranoid Android.
 
-#include <android-base/properties.h>
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstdlib>
+#include <string.h>
+
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <sys/sysinfo.h>
+#include <android-base/properties.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
 
 using android::base::GetProperty;
+using std::string;
 
-void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info* pi;
+// List of partitions to override props
+static const string source_partitions[] = {
+    "", "bootimage.", "odm.", "product.",
+    "system.", "system_ext.", "vendor."
+};
 
-    pi = (prop_info*)__system_property_find(prop);
-    if (pi)
+void property_override(char const prop[], char const value[]) {
+    auto pi = (prop_info*) __system_property_find(prop);
+
+    if (pi != nullptr)
         __system_property_update(pi, value, strlen(value));
-    else if (add)
+    else
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void set_ro_build_prop(const string &prop, const string &value) {
+    string prop_name;
+
+    for (const string &source : source_partitions) {
+        prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str());
+    }
+}
+
+void set_device_props(const string brand, const string device,
+        const string model, const string name, const string marketname) {
+    set_ro_build_prop("brand", brand);
+    set_ro_build_prop("device", device);
+    set_ro_build_prop("model", model);
+    set_ro_build_prop("name", name);
+    set_ro_build_prop("marketname", marketname);
+    property_override("bluetooth.device.default_name", marketname.c_str());
 }
 
 void load_dalvikvm_properties() {
@@ -44,15 +89,21 @@ void load_dalvikvm_properties() {
 }
 
 void vendor_load_properties() {
-    property_override("ro.product.brand", "Xiaomi");
-    property_override("ro.product.device", "psyche");
-    property_override("ro.product.manufacturer", "Xiaomi");
-    property_override("ro.product.model", "2112123AG");
-    property_override("ro.product.name", "psyche");
+    // Detect variant and override properties
+    string region = GetProperty("ro.boot.hwc", "");
+
+    if (region == "Global") { // China
+        set_device_props("Xiaomi", "psyche", "2112123AG", "psyche_global", "Xiaomi 12X");
+    } else { // Global
+        set_device_props("Xiaomi", "psyche", "2112123AC", "psyche", "Xiaomi 12X");
+    }
 
     // sku properties
     property_override("ro.boot.hardware.sku", "psyche");
     property_override("ro.boot.product.hardware.sku", "psyche");
+
+    // Set hardware revision
+    property_override("ro.boot.hardware.revision", GetProperty("ro.boot.hwversion", "").c_str());
 
     load_dalvikvm_properties();
 }
