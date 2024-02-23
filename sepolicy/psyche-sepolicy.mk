b8fdf2d2f4c7b41ab6fd5ad07eb6f2a17d16d6e8
Mesquita <mesquita@aospa.co>
2024-02-23 11:08:49 -0300

    psyche: sepolicy: Switch to xiaomi common sepolicy
    Change-Id: Ic142cec173e01e94838e5c121c192bab36764abd
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- sepolicy/psyche-sepolicy.mk
+++ sepolicy/psyche-sepolicy.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Public Sepolicy
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/xiaomi/psyche/sepolicy/public
-
 # QCOM Sepolicy
 BOARD_SEPOLICY_DIRS += \
     device/xiaomi/psyche/sepolicy/vendor/qcom
@@ -24,12 +20,10 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_DIRS += \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/audio \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/bluetooth \
-    device/xiaomi/psyche/sepolicy/vendor/xiaomi/battery \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/camera \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/display \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/dolby \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/fingerprint \
-    device/xiaomi/psyche/sepolicy/vendor/xiaomi/ir \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/modem \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/power_supply \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/sensors \
@@ -37,5 +31,4 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/touch \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/touchfeature \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/usb \
-    device/xiaomi/psyche/sepolicy/vendor/xiaomi/wireless \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/wlan
