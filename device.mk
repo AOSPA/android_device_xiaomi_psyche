396e39d2aa3344491c3adec0b6c9ebf0e947bd5d
Mesquita <mesquita@aospa.co>
2024-03-29 01:10:45 -0300

    psyche: Add Mlipay to Support Fingerprint Payment
     * Manifest entries are at https://github.com/AOSPA/android_hardware_xiaomi/blob/uvite/vintf/xiaomi_framework_compatibility_matrix.xml
       - vendor.xiaomi.hardware.mlipay
       - vendor.xiaomi.hardware.mfidoca
       - vendor.xiaomi.hardware.mtdservice
       - vendor.xiaomi.hardware.tidaservice
    
    Change-Id: I94a929687940f5346daa2fb3314ec7577aa85f9d
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -236,6 +236,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
